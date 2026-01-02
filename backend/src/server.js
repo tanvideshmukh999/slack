a96dced02b83c072f555d4eaf9d8fc036c853849
Tanvi Deshmukh <tanvideshmukh999@gmail.com>
2026-01-02 06:40:02 +0530

    db connected & env.js completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app=express();
 
@@ -9,7 +10,12 @@ app.get("/",(req,res)=>{
     res.send("Hello world");
 })
 
-console.log("mongo uri:",ENV.MONGO_URI)
 
-app.listen(ENV.PORT,()=>console.log("server started on port:",ENV.PORT));
 
+app.listen(ENV.PORT,()=>{
+console.log("server started on port:",ENV.PORT);
+connectDB()
+})
+    
+
+//knRI2mEV9JajVX3s
