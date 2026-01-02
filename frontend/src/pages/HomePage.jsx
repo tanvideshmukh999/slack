bb701153c4705a2f159fdb675956372e03369920
Tanvi Deshmukh <tanvideshmukh999@gmail.com>
2026-01-02 14:59:02 +0530

    auth page implemented

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import { UserButton } from '@clerk/clerk-react'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div><UserButton/>
+    Home page </div>
+  )
+}
+
+export default HomePage
